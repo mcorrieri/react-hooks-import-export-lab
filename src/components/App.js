eb6286cdea3f7e674a2c46d86b952fb26a234749
Michael <Mlcorrieri@gmail.com>
2021-04-05 17:33:26 -0400

    done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import About from "../components/About";
+import NavBar from "../components/NavBar";
+import Home from "../components/Home";
 
 function App() {
   return (
